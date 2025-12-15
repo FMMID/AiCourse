5121aa8d0c1258779e96b51afddf6ac31156d0c7
MikhailFedotov <fedotov.mihail.m@gmail.com>
2025-12-15 23:27:01 +0300

    init mcpclient module

--- mcpclient/build.gradle.kts
+++ mcpclient/build.gradle.kts
@@ -1,12 +1,27 @@
 plugins {
     alias(libs.plugins.jetbrains.kotlin.jvm)
+    id("java-library")
 }
 
-// Настройка версии Java
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
 }
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+    }
+}
+
 
 dependencies {
+    api(libs.mcp.kotlin.sdk)
+
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.kotlinx.json)
+
+//    implementation(libs.mcp.anthropic)  //TODO пока используем для примера
+//    implementation(libs.mcp.logging) //TODO пока используем для примера
 }
