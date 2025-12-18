564100800ecba473f6cb0f51976e552977df31da
MikhailFedotov <fedotov.mihail.m@gmail.com>
2025-12-18 18:55:17 +0300

    init notification mcp server

---
+++ AiCourseNotificationBackend/settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "notification"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ AiCourseNotificationBackend/src/main/kotlin/com/example/aicourse/backend/notification/Routing.kt
@@ -0,0 +1,6 @@
+package com.example.aicourse.backend.notification
+
+import io.ktor.server.application.*
+
+fun Application.configureRouting() {
+}
